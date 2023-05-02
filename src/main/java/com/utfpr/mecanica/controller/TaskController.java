4e7c644d18451a4b354a1f9f106b07e2a7669ebd
NewDaniC <danieltheisgesdacosta@gmail.com>
2023-05-02 11:47:30 -0300

    thymeleaf e index.html

---
+++ src/main/java/com/utfpr/mecanica/controller/TaskController.java
@@ -0,0 +1,18 @@
+package com.utfpr.mecanica.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TaskController {
+	
+	@GetMapping("/")
+	public String index() {
+	    return "index";
+	}
+	
+	@GetMapping("/create")
+	public String create() {
+	    return "create";
+	}
+}
